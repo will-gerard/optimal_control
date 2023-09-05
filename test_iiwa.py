6134ca4ac0433a19831ec58a56e63905aa0dddaa
William Gerard <whgerard1@gmail.com>
2023-09-05 15:53:50 -0400

    Update cost function to better reflect HAL paper
    See https://hal.science/hal-02993058v2/document for the paper
    I'm talking about, High-Frequency Nonlinear Model Predictive
    Control of a Manipulator. I want a cost function close to the
    one they define in equations 9 and 10.

--- test_iiwa.py
+++ test_iiwa.py
@@ -3,6 +3,10 @@ import pinocchio
 
 import example_robot_data
 
+# load the talos model from example robot data
+robot = example_robot_data.load("talos_arm")
+robot_model = robot.model
+
 import crocoddyl
 import os
 print("Current Working Directory:", os.getcwd())
@@ -21,8 +25,8 @@ pinocchio.forwardKinematics(robot.model, robot.data, q)
 # grid compute end effector location is run from the same starting position
 joint_7_index = robot.model.getFrameId("iiwa_joint_7")
 
-DT = 1e-3
-KNOT_POINTS = 32
+DT = .03
+KNOT_POINTS = 128
 NUM_CONTROLS = 7
 
 cameraTF = [2.0, 2.68, 0.54, 0.2, 0.62, 0.72, 0.22]
@@ -52,25 +56,40 @@ print(h_xu_traj)
 
 # create x0, the starting state, the first 14 elements of the flattened xu_traj
 x0 = h_xu_traj[:14]
+u0 = h_xu_traj[14:21]
 
 state_differential_model = crocoddyl.StateMultibody(robot_model)
 
 # Initialize matrices Q and R for state and control tracking cost
-Q_COST = 0.1
-R_COST = 0.0001
+Q1_COST = 0.1
+Q2_COST = 1.0
+R_COST = 0.1
+
+QF_COST = 10000.0
+QF = np.full((state_differential_model.nx, 1), QF_COST)
 # set Q to a column vector of length state_differential_model.nx and value 0.1
 
-Q = np.full((state_differential_model.nx, 1), Q_COST)
+# set Q to column vector of length state_differential_model.nx, first 7 values are Q1_COST, last 7 values are Q2_COST
+Q = np.full((state_differential_model.nx, 1), Q1_COST)
+Q[7:] = Q2_COST
+
+# Q = np.full((state_differential_model.nx, 1), Q_COST)
 R = np.full((NUM_CONTROLS, 1), R_COST)
 
 # EE cost multiplier is hardcoded to 0.5
 COST_MATRIX_EE = np.full((3, 1), 0.5)
+
+W_GOAL_TRACK_RESIDUAL = 10.0
 
 # 1. Update desired state and control sequences based on the current state
 # x_des, u_des = get_desired_trajectory_slice(x0, N)  # Implement this function based on your application
 # for now only look at the first 32 points
 target_traj_x = xu_traj2d[0:KNOT_POINTS, 0:14]
 target_traj_u = xu_traj2d[0:KNOT_POINTS, 14:]
+
+# create lists of each nd array for each of the target trajectories as well
+initial_xs = [target_traj_x[i] for i in range(KNOT_POINTS)]
+initial_us = [target_traj_u[i] for i in range(KNOT_POINTS-1)]
 
 target_ee_traj = eePos_traj2d[0:KNOT_POINTS]
 
@@ -82,7 +101,9 @@ terminalCostModel = crocoddyl.CostModelSum(state_differential_model)
 for t in range(KNOT_POINTS):
     x_ref_t = target_traj_x[t]
     u_ref_t = target_traj_u[t]
-    ee_pos_ref_t = target_ee_traj[t]
+    # also create a zeros u target
+    u_ref_t_zeros = np.zeros(NUM_CONTROLS)
+    ee_pos_ref_t = target_ee_traj[KNOT_POINTS-1]
     
     # per https://gepettoweb.laas.fr/doc/loco-3d/crocoddyl/master/doxygen-html/classcrocoddyl_1_1CostModelResidualTpl.html#details
     # ActivationModelQuadTpl is used by default
@@ -93,30 +114,37 @@ for t in range(KNOT_POINTS):
     state_error_residual = crocoddyl.ResidualModelState(state_differential_model, x_ref_t)
     state_tracking_cost = crocoddyl.CostModelResidual(state_differential_model, activation_state, state_error_residual)
 
-    # don't actually add this to running cost for now, we don't include state offset in the loss function in gpu pcg tracking
-    # runningCostModel.addCost("stateTrack" + str(t), state_tracking_cost, 1.)
+    # # don't actually add this to running cost for now, we don't include state offset in the loss function in gpu pcg tracking
+    runningCostModel.addCost("stateTrack" + str(t), state_tracking_cost, 1.)
     
-    activation_ee = crocoddyl.ActivationModelWeightedQuad(COST_MATRIX_EE)
+    # activation_ee = crocoddyl.ActivationModelWeightedQuad(COST_MATRIX_EE)
     goalTrackingCostXYZ = crocoddyl.CostModelResidual(
         state_differential_model,
-        activation_ee,
+        # activation_ee, # default activation model is quadratic: https://gepettoweb.laas.fr/doc/loco-3d/crocoddyl/master/doxygen-html/classcrocoddyl_1_1CostModelResidualTpl.html
         crocoddyl.ResidualModelFrameTranslation(
             state_differential_model, joint_7_index, ee_pos_ref_t[0:3]
         ),
     )
-    runningCostModel.addCost("goalTrackXYZ" + str(t), goalTrackingCostXYZ, 1.)
+    runningCostModel.addCost("goalTrackXYZ" + str(t), goalTrackingCostXYZ, W_GOAL_TRACK_RESIDUAL)
 
     # add control tracking cost if we are not at the last knot point
     if t < KNOT_POINTS - 1:
         activation_control = crocoddyl.ActivationModelWeightedQuad(R)
         control_error_residual = crocoddyl.ResidualModelControl(state_differential_model, u_ref_t)
         control_tracking_cost = crocoddyl.CostModelResidual(state_differential_model, activation_control, control_error_residual)
-        runningCostModel.addCost("ctrlTrack" + str(t), control_tracking_cost, 0.001)
+        runningCostModel.addCost("ctrlTrack" + str(t), control_tracking_cost, 1.)
+    else:
+        activation_state_final = crocoddyl.ActivationModelWeightedQuad(QF)
+        state_error_residual_final = crocoddyl.ResidualModelState(state_differential_model, x_ref_t)
+        state_tracking_cost_final = crocoddyl.CostModelResidual(state_differential_model, activation_state_final, state_error_residual_final)
+
+        # without the terminal cost, the final trajectory ends up being bad
+        terminalCostModel.addCost("stateTrack", state_tracking_cost_final, 1.)
 
 x_ref_t = target_traj_x[-1]
 u_ref_t = np.zeros(NUM_CONTROLS)
 
-# terminalCostModel.addCost("stateTrack", state_tracking_cost, 10000)
+
 
 
 running_model = crocoddyl.IntegratedActionModelEuler(
@@ -129,10 +157,10 @@ crocoddyl.DifferentialActionModelFreeFwdDynamics(
     state_differential_model, actuation, terminalCostModel)
 )
 
-problem = crocoddyl.ShootingProblem(x0, [running_model] * KNOT_POINTS, terminal_model)
+problem = crocoddyl.ShootingProblem(x0, [running_model] * (KNOT_POINTS-1), terminal_model)
 
 # Creating the DDP solver for this OC problem, defining a logger
-ddp = crocoddyl.SolverDDP(problem)
+ddp = crocoddyl.SolverFDDP(problem)
 ddp.setCallbacks([crocoddyl.CallbackVerbose()])
 
 # time how long the ddp solve takes
@@ -140,7 +168,7 @@ import time
 time_limit = 0.01
 start_time = time.time()
 # Solving it with the DDP algorithm
-ddp.solve()
+ddp.solve(init_xs=initial_xs, init_us=initial_us, maxiter=100, is_feasible=False)
 end_time = time.time()
 print("DDP solve time: ", end_time - start_time)
 
@@ -160,6 +188,8 @@ print(x_ref_t)
 # print final x
 print("And the final x:")
 print(xT)
+
+print(f"Total cost: {ddp.cost}")
 
 # Actual tracking stuff, just going to try a single solve for now
 # 4. Apply the first control to your system
