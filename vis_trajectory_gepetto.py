09a7bf47ec48f0a32fb850ef7f216fb28c431897
William Gerard <whgerard1@gmail.com>
2023-09-06 11:43:30 -0400

    Update visualization script to include path

--- vis_trajectory_gepetto.py
+++ vis_trajectory_gepetto.py
@@ -19,11 +19,17 @@ robot: pinocchio.RobotWrapper = load_robot_description("iiwa_description")
 robot_model = robot.model
 
 cameraTF = [2.0, 2.68, 0.54, 0.2, 0.62, 0.6, 0.22]
-display: crocoddyl.GepettoDisplay = crocoddyl.GepettoDisplay(robot, cameraTF=cameraTF, floor=False)
+display: crocoddyl.GepettoDisplay = crocoddyl.GepettoDisplay(robot, cameraTF=cameraTF, floor=False, frameNames=[])
 robot.viewer.gui.refresh()
 
 xu_traj_file_name = f"testfiles/0_0_traj.csv"
 xu_traj2d = np.genfromtxt(xu_traj_file_name, delimiter=',')
+
+eePos_traj_file_name = f"testfiles/0_0_eepos.traj"
+eePos_traj2d = np.genfromtxt(eePos_traj_file_name, delimiter=',')
+
+# create list of end effector positions
+ee_pos_seq = [traj[0:3].tolist() for traj in eePos_traj2d]
 
 # update the display to put the robot at the starting position
 q_vals = xu_traj2d[0][0:robot.nq]
@@ -31,6 +37,27 @@ display.robot.display(q_vals)
 
 # sleep for a bit to let the display update
 time.sleep(1)
+key = 'iiwa_joint_7'
+color = np.hstack([np.random.choice(range(256), size=3) / 256.0, 1.0]).tolist()
+print(f"using color {color}")
+frameName = display.frameTrajGroup + "/" + key
+display.robot.viewer.gui.addCurve(
+    frameName,
+    ee_pos_seq,
+    color,
+)
+display.robot.viewer.gui.setCurveLineWidth(frameName, display.frameTrajLineWidth)
+if display.fullVisibility:
+    display.robot.viewer.gui.setVisibility(frameName, "ALWAYS_ON_TOP")
+
+robot.viewer.gui.refresh()
+time.sleep(1)
+
+robot.viewer.gui.createGroup("frame")
+robot.viewer.gui.addToGroup("frame", "crocoddyl")
+robot.viewer.gui.addLandmark("frame", 0.01)
+robot.viewer.gui.applyConfiguration("frame", [0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 1.0])
+robot.viewer.gui.refresh()
 
 # iterate over the x values
 for i in range(0, len(xu_traj2d)):
@@ -42,4 +69,4 @@ for i in range(0, len(xu_traj2d)):
     robot.viewer.gui.refresh()
     time.sleep(0.01)
 
-
+display._addFrameCurves()
